7afc825a5bbbd7b8a6885c8eeb7e4edb6407bfe1
Daniel Jurek <daniel@octaviuslabs.com>
2015-03-11 17:20:54 -0700

    Routes for Active and Completed

--- js/router.js
+++ js/router.js
@@ -1,8 +1,32 @@
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function() { 
-
+    this.route('active'); 
+    this.route('completed'); 
   });
 });
+
+Todos.TodosActiveRoute = Ember.Route.extend({ 
+  model: function() { 
+    return this.store.filter('todo', function(todo) { 
+      return !todo.get('isCompleted'); 
+    }); 
+  }, 
+  renderTemplate: function(controller) { 
+    this.render('todos/index', {controller: controller}); 
+  }
+}); 
+
+Todos.TodosCompletedRoute = Ember.Route.extend({ 
+  model: function() { 
+    return this.store.filter('todo', function(todo) { 
+      return todo.get('isCompleted'); 
+    }); 
+  }, 
+  renderTemplate: function(controller) { 
+    this.render('todos/index', {controller: controller}); 
+  }
+}); 
+
 
 Todos.TodosIndexRoute = Ember.Route.extend({ 
   model: function() { 
